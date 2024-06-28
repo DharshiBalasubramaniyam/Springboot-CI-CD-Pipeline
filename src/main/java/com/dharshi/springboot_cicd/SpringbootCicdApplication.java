aa7b9ec721b2cc48119c2f8c88f5bd810b583ebe
dharshib <Dharshib.8@gmail.com>
2024-06-28 14:27:54 +0530

    initialized springboot project

---
+++ src/main/java/com/dharshi/springboot_cicd/SpringbootCicdApplication.java
@@ -0,0 +1,13 @@
+package com.dharshi.springboot_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dharshi/springboot_cicd/SpringbootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharshi.springboot_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
