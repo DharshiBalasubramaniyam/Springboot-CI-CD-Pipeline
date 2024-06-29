5e21a02aba11849825ab5e66fd55e19998fbf268
Dharshi Balasubramaniyam <139672976+DharshiBalasubramaniyam@users.noreply.github.com>
2024-06-29 19:54:26 +0530

    Update Controller.java

--- src/main/java/com/dharshi/springboot_cicd/controllers/Controller.java
+++ src/main/java/com/dharshi/springboot_cicd/controllers/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/home")
+    @GetMapping("/welcome")
     public ResponseEntity<String> welcome() {
         return ResponseEntity.ok("Welcome to Spring Boot CI/CD Pipeline!");
     }
