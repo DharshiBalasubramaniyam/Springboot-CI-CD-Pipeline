4f7d87c36ab01ffd05f7d0874440d509a4a9e746
dharshib <Dharshib.8@gmail.com>
2024-06-30 16:51:13 +0530

    added crossorigin

--- src/main/java/com/dharshi/springboot_cicd/controllers/Controller.java
+++ src/main/java/com/dharshi/springboot_cicd/controllers/Controller.java
@@ -1,15 +1,17 @@
 package com.dharshi.springboot_cicd.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
 
     @GetMapping("/home")
     public ResponseEntity<String> home() {
-        return ResponseEntity.ok("Welcome to Spring Boot CI/CD Pipeline with GitHub Actions, Docker and AWS ECS!");
+        return ResponseEntity.ok("Hello, Welcome to Spring Boot CI/CD Pipeline with GitHub Actions, Docker and AWS ECS!");
     }
 
 }
